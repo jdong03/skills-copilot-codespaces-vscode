3348ee86523d3157e7c134a13460c25615adf667
jdong03 <40895702+jdong03@users.noreply.github.com>
2023-06-09 15:53:43 +0000

    fix function name

--- skills.js
+++ skills.js
@@ -1,3 +1,11 @@
-function calculateNumber(var1, var2) {
+function calculateNumbers(var1, var2) {
   return Math.round(var1) + Math.round(var2);
+}
+
+function differenceNumber(var1, var2) {
+  return Math.round(var1) - Math.round(var2);
+}
+
+function productNumber(var1, var2) {
+  return Math.round(var1) * Math.round(var2);
 }
